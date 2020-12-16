5f70bd1c60d54925124f3657ba448d91bd709b3e
Umoren <samuelumoren365@gmail.com>
2020-12-16 17:32:06 +0100

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="layout">
+        <header class="header">
+            <Navbar />
+        </header>
+
+        <slot />
+    </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+import Navbar from '~/components/Navbar';
+export default {
+    components: {
+        Navbar,
+    },
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Karla&display=swap');
+body {
+    font-family: 'Karla', sans-serif;
+    margin: 0;
+    padding: 0;
+    background: #753ff6;
+    line-height: 1.5;
+}
+
+.layout {
+    max-width: 950px;
+    margin: 0 auto;
+    padding-left: 20px;
+    padding-right: 20px;
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    height: 80px;
+}
+
+.nav__link {
+    margin-left: 20px;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,45 @@
+<template>
+    <Layout>
+        <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+        <section class="hero-section">
+            <div>
+                <h1>Welcome!</h1>
+
+                <p>
+                    Strapi is the leading open-source headless CMS. It’s 100%
+                    Javascript, fully customizable and developer-first.
+                </p>
+
+                <p>Enjoy our courses here</p>
+            </div>
+        </section>
+
+        <section>
+            <AllCourses />
+        </section>
+    </Layout>
+</template>
+
+<script>
+import AllCourses from '~/components/AllCourses';
+export default {
+    metaInfo: {
+        title: 'Learning Platform',
+    },
+    components: {
+        AllCourses,
+    },
+};
+</script>
+
+<style>
+.home-links a {
+    margin-right: 1rem;
+}
+
+.hero-section {
+    background: #fff;
+    color: #2f2e8b;
+    padding: 1rem;
+}
+</style>
