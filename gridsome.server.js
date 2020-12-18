07eb22031931f644658368db901750eba8bf6053
Umoren <samuelumoren365@gmail.com>
2020-12-18 12:48:00 +0100

    removed env

--- gridsome.server.js
+++ gridsome.server.js
@@ -10,7 +10,7 @@ const axios = require('axios')
 module.exports = function (api) {
 
     api.loadSource(async actions => {
-      const { data } = await axios.get(`${process.env.GRIDSOME_STRAPI_URL}/learning-platforms/`)
+      const { data } = await axios.get(`https://aqueous-bastion-06425.herokuapp.com/learning-platforms/`)
 
       const collection = actions.addCollection({
         typeName: 'Course',
